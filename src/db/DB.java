901a821640125dc100bdc21321d4b80ee0eb0fa3
Rubens Soares <rubens.soares@liferay.com>
2024-03-18 13:18:24 -0300

    Reusing instantiation

--- src/db/DB.java
+++ src/db/DB.java
@@ -38,7 +38,7 @@ public class DB {
 	}
 	
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
+		try (FileInputStream fs = new FileInputStream("/home/me/demo-dao-jdbc/demo-dao-jdbc/db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
